da2632ac55803dfd930fcb1f39780b59412e02d6
SimonYung <simonquere24@gmail.com>
2022-04-06 17:06:39 +0200

    upadted landing component and its css

--- _includes/components/landing.js
+++ _includes/components/landing.js
@@ -1,16 +1,16 @@
 exports.render = function ( content , context )
 {
 	let output = `
-	<section class="header" style="background-image: url('${context.imgCopy(content.background).url}');">
-		<div class="header__frame">
-			<h1 class="header__title">${content.title}</h1>
-			<div class="header__subTitle">
-				<p class="header__description">${content.subTitle}</p>`;
+	<section class="landing__background" style="background-image: url('${context.imgCopy(content.background).url}');">
+		<div class="landing__content">
+			<h1 class="landing__content__title">${content.title}</h1>
+			<div class="landing__content__description">
+				<span class="landing__content__description__text">${content.subTitle}</span>`;
 	let i = 0;
 	while (content.button[i] != undefined)
 	{
 		output += `
-				<a href="${content.button[i].href}" class="header__callToAction">${content.button[i].text}</a>`;
+				<a href="${content.button[i].href}" class="landing__content__description__callToAction">${content.button[i].text}</a>`;
 		i++;
 	}
 	output +=`
