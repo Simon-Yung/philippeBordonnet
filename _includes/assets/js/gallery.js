b32c85fe3aa95cadc79f5ac103ad618928c89066
SimonYung <simonquere24@gmail.com>
2022-04-09 17:12:04 +0200

    moved collection1 data from csv to yaml, improved the gallery generation

--- _includes/assets/js/gallery.js
+++ _includes/assets/js/gallery.js
@@ -5,6 +5,7 @@ let preview;
 let title;
 let description;
 let text;
+let price;
 
 function init()
 {
@@ -13,9 +14,11 @@ function init()
 	preview.style.transition = '0.3s all ease 0.0s';
 	text = document.getElementById('text');
 	title = document.getElementById('title');
+	price = document.getElementById('price');
 	description = document.getElementById('description');
 
-	let articles = document.querySelectorAll('article');
+	//let articles = document.querySelectorAll('article');
+	let articles = document.getElementsByClassName('grid__article__img');
 	for (i = 0; i < articles.length; i++)
 	{
 		articles[i].addEventListener('pointerup' , updateImage);
@@ -28,10 +31,11 @@ function updateImage(e)
 	e.target.tagname == "ARTICLE" ? dom = e.target : dom = e.target.parentElement;
 	preview.src = dom.dataset.imgUrl;
 	preview.style.width = `calc( ${parseInt(dom.dataset.imgWidth)}% - 3rem )`;
-	preview.style.filter = 'blur(5px) opacity(0.2)';
-	preview.addEventListener( 'load' , (e) => {e.target.style.filter = 'blur(0px) opacity(1.0)'} , {once: true} )
+	preview.style.filter = 'blur(5px) opacity(0.2) brightness(0.0)';
+	preview.addEventListener( 'load' , (e) => {e.target.style.filter = 'blur(0px) opacity(1.0) brightness(1.0)'} , {once: true} )
 	text.style.width = `calc( ${100 - parseInt(dom.dataset.imgWidth)}% - 3rem );`;
-	title.innerHTML = dom.dataset.title;
-	description.innerHTML = dom.dataset.description;
+	title.innerHTML = dom.dataset.title?dom.dataset.title:'';
+	price.innerHTML = dom.dataset.price?dom.dataset.price:'';
+	description.innerHTML = dom.dataset.desc?dom.dataset.desc:'';
 	window.scrollTo(0,0);
 }
--- _includes/components/gallery.js
+++ _includes/components/gallery.js
@@ -7,26 +7,53 @@ exports.render = function ( content, pagedata )
 				<img id="preview" class="textAndImage__image" style="width: calc( ${content.default.imageWidth}% - 3rem );" src="${this.imgCopy(content.default.image).url}">
 
 				<article id="text" class="textAndImage__description" style="width: calc( ${100 - content.default.imageWidth}% - 3rem );">
-					<div class="textAndImage__banner"><h1 id="title">${content.default.title}</h1></div>
-					<p id="description" >${content.default.description.replace(/\n/g,'<br>')}</p>
+					<div class="textAndImage__banner"><h1 id="title" style="font-size:2rem">${content.default.title}</h1></div>
+					<h3 id="price"></h3>
+					<p id="description" style="line-height:2.0">${content.default.description.replace(/(\r\n|\r|\n)/g,'<br>')}</p>
 				</article>
 			</section>
 			<section class="grid">`
 	let i = 0;
+	let quickDesc = '';
+	switch ( pagedata.page.local )
+	{
+		case 'en-GB' :
+			quickDesc = 'Work Hand-signed by the Artist<br>Certificate of Authenticity';
+			break;
+		case 'fr-FR' :
+			quickDesc = 'Oeuvre signée à la main par l\'Artiste<br>Avec Certificat d\'Authenticité';
+			break;
+		case 'de-DE' :
+			quickDesc = 'Handsigniert vom künstler<br>Echtheitsbescheinigung';
+			break;
+		default :
+			quickDesc = 'Work Hand-signed by the Artist<br>Certificate of Authenticity';
+			break;
+	}
 	while ( Boolean(pagedata.artworkCollections[content.collection][i]) )
 	{
-		let img = this.imgCreate(pagedata.artworkCollections[content.collection][i].image, 500);
+		let currentWork = pagedata.artworkCollections[content.collection][i];
+		let img = this.imgCreate(currentWork.image, 500);
 		output += `
 				<article class="grid__article"
-				data-img-url="${img.OGUrl}" 
-				data-img-width="${pagedata.artworkCollections[content.collection][i].imageWidth}"
-				data-title="${pagedata.artworkCollections[content.collection][i].title}"
-				data-description="${pagedata.artworkCollections[content.collection][i].longDescription.replace(/\\n/g,'<br>')}">
+				data-thumb-url="${img.thumbnailUrl}"
+				data-img-url="${img.OGUrl}"
+				data-img-width="${currentWork.imageWidth?currentWork.imageWidth:'60'}"
+				data-title="${currentWork.title?currentWork.title:''}"
+				data-dimmensions="${currentWork.dimmensions?currentWork.dimmensions:''}"
+				data-year="${currentWork.year?currentWork.year:''}"
+				data-price="${currentWork.price?currentWork.price:''}"
+				data-desc="${currentWork.description[pagedata.page.local]?currentWork.description[pagedata.page.local].replace(/(\r\n|\r|\n)/g,'<br>'):''}">
 					<img class="grid__article__img" src="${img.thumbnailUrl}" 
 					loading="lazy"
 					style="aspect-ratio: 100/${img.heightRatio}">
-					<strong>${pagedata.artworkCollections[content.collection][i].title}</strong>
-					<p>${pagedata.artworkCollections[content.collection][i].shortDescription.replace(/\\n/g,'<br>')}</p>
+					<strong>${currentWork.title}</strong>
+					<p>
+					${currentWork.dimmensions}<br>
+					Created in ${currentWork.year}<br>
+					${quickDesc}</p>
+					<!-- <p>
+					${currentWork.year?currentWork.year:''} <br> ${currentWork.dimmensions?currentWork.dimmensions:''}</p> -->
 				</article>`
 		++i
 	}
